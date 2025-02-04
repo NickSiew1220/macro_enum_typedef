33361c7a40fe1b10e2477d34fa70ec2035e9c7d0
NickSiew1220 <156846649+NickSiew1220@users.noreply.github.com>
2025-02-04 16:13:12 -0500

    part 1 and 2 done
    h

---
+++ pgm_macro_enum/Debug/pgm_macro_enum_main.d
@@ -0,0 +1,19 @@
+# FIXED
+
+pgm_macro_enum_main.obj: ../pgm_macro_enum_main.c
+pgm_macro_enum_main.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdio.h
+pgm_macro_enum_main.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/_ti_config.h
+pgm_macro_enum_main.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/linkage.h
+pgm_macro_enum_main.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdarg.h
+pgm_macro_enum_main.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/_types.h
+pgm_macro_enum_main.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/cdefs.h
+pgm_macro_enum_main.obj: C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine/_types.h
+
+../pgm_macro_enum_main.c: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdio.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/_ti_config.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/linkage.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/stdarg.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/_types.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/sys/cdefs.h: 
+C:/ti/ccs1000/ccs/tools/compiler/ti-cgt-arm_20.2.0.LTS/include/machine/_types.h: 
---
+++ pgm_macro_intro/Debug/pgm_macro_intro_main.d
@@ -0,0 +1,5 @@
+# FIXED
+
+pgm_macro_intro_main.obj: ../pgm_macro_intro_main.c
+
+../pgm_macro_intro_main.c: 
